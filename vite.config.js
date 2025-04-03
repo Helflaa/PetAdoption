4e78f557288abf71b67340335bff88c66870aa35
Helflaa <146756162+Helflaa@users.noreply.github.com>
2025-04-03 15:23:52 +0200

    Feat: created route for every page

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,12 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: path.resolve(__dirname, "index.html"),
-                login: path.resolve(__dirname, "login/index.html"),
-                register: path.resolve(__dirname, "register/index.html"),
+                login: path.resolve(__dirname, "account/login/index.html"),
+                register: path.resolve(__dirname, "account/register/index.html"),
                 admin: path.resolve(__dirname, "admin/index.html"),
                 pet: path.resolve(__dirname, "pet/index.html"),
+                create: path.resolve(__dirname, "pet/create/index.html"),
+                edit: path.resolve(__dirname, "pet/edit/index.html"),
             },
         },
     },
